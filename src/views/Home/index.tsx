599867b31a7c4c01b0ae2cd3928260e376bc4c3e
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-06-30 13:32:19 -0300

    style: add styles in home

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 import * as S from "./styled";
 import { getFilms } from "../../server/index";
-import { Loading } from "../../components";
+import { Loading, Poster } from "../../components";
+import { PostersStarWarsFilms } from "../../global/const";
 
 const Home: React.FC = () => {
-  const [films, setFilms] = useState(" ");
+  const [films, setFilms] = useState<any>();
+
+  const getImagePoster = (title: string) => {
+    const keys = Object.keys(PostersStarWarsFilms);
+
+    const formattedTitle = title.replace(/\s+/g, "");
+
+    for (const key of keys) {
+      const formattedKey = key.replace(/\s+/g, "");
+
+      if (formattedTitle.toLowerCase().includes(formattedKey.toLowerCase())) {
+        return PostersStarWarsFilms[key];
+      }
+    }
+
+    return PostersStarWarsFilms.No_Image;
+  };
 
   useEffect(() => {
     async function fetchFilms(): Promise<void> {
@@ -17,7 +34,28 @@ const Home: React.FC = () => {
     }
     fetchFilms();
   }, []);
-  return <>{console.log("FILMs", films)}</>;
+  return (
+    <>
+      {films ? (
+        <S.ContainerPosters>
+          {films
+            .sort((a: any, b: any) => a.episode_id - b.episode_id)
+            .map((film: any) => (
+              <S.LinkRedirect
+                to={`/film/${film.url.split("/").filter(Boolean).pop()}`}
+              >
+                <Poster
+                  image={getImagePoster(film.title)}
+                  description={film.title}
+                />
+              </S.LinkRedirect>
+            ))}
+        </S.ContainerPosters>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 };
 
 export default Home;
--- src/views/Home/styled.tsx
+++ src/views/Home/styled.tsx
@@ -1,5 +1,19 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const ContainerPosters = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 20px;
+  padding: 20px;
+  justify-content: center;
+`;
 
 export const Text = styled.h1`
   color: white;
+`;
+
+export const LinkRedirect = styled(Link)`
+  text-decoration: none;
 `;

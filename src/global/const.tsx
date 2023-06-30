18f7c8a85a6257ebdf218d03ea101bed87ca4673
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-06-30 13:31:16 -0300

    feat: add const global in project add sound intro star wars

---
+++ src/global/const.tsx
@@ -0,0 +1,16 @@
+export const PostersStarWarsFilms: any = {
+  ThePhantomMenace:
+    "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.6763_AL_.jpg",
+  AttackoftheClones:
+    "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_Ratio0.6763_AL_.jpg",
+  RevengeoftheSith:
+    "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_Ratio0.7536_AL_.jpg",
+  ANewHope:
+    "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_Ratio0.6762_AL_.jpg",
+  TheEmpireStrikesBack:
+    "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6763_AL_.jpg",
+  ReturnoftheJedi:
+    "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.6763_AL_.jpg",
+  NoImage:
+    "https://s2.glbimg.com/xIK2tpANlRYKSIIrUYEnndmeW4s=/smart/e.glbimg.com/og/ed/f/original/2015/11/17/darth-vader-star-wars.jpg",
+};

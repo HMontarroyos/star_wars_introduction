07731b92a7ec04979447ca3cef8bc2044958c1fc
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-07-02 17:59:37 -0300

    feat: add animation in text intro star wars and add sound

--- src/components/StarWarsIntro/index.tsx
+++ src/components/StarWarsIntro/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styled";
 
 type StarWarsIntroProps = {
@@ -6,23 +6,69 @@ type StarWarsIntroProps = {
 };
 
 const StarWarsIntro = ({ description }: StarWarsIntroProps) => {
+  const [showIntro, setShowIntro] = useState(true);
+  const [audioStarted, setAudioStarted] = useState(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!showIntro && !audioStarted) {
+      const audioInstance = new Audio(require("../../sounds/starwars.mp3"));
+      setAudio(audioInstance);
+      audioInstance.play();
+      setAudioStarted(true);
+    }
+
+    return () => {
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    };
+  }, [showIntro, audioStarted, audio]);
+
   const paragraphs = description.split(/(?<=\.)\s{2,}/);
+
+  useEffect(() => {
+    if (!showIntro && audioStarted && !fade) {
+      const fadeTimer = setTimeout(() => {
+        setFade(true);
+      }, 5000);
+
+      return () => clearTimeout(fadeTimer);
+    }
+  }, [showIntro, audioStarted, fade]);
 
   return (
     <>
-      <S.TextIntro>
-        A Long Time Ago, in a galaxy far,
-        <br />
-        far away ...
-      </S.TextIntro>
-      <S.ContainerParagraph>
-        {paragraphs.map((paragraph, idx) => (
-          <>
-            <S.Paragraph key={idx}>{paragraph}</S.Paragraph>
-            <br />
-          </>
-        ))}
-      </S.ContainerParagraph>
+      {showIntro ? (
+        <S.TextIntro>
+          A Long Time Ago, in a galaxy far,
+          <br />
+          far away ...
+        </S.TextIntro>
+      ) : null}
+
+      {!showIntro && (
+        <S.ContainerParagraph fade={fade}>
+          {paragraphs.map((paragraph, idx) => (
+            <React.Fragment key={idx}>
+              <S.Paragraph /* style={{ animationDelay: `${idx * 0.5}s` }} */>
+                {paragraph}
+              </S.Paragraph>
+              <br />
+            </React.Fragment>
+          ))}
+        </S.ContainerParagraph>
+      )}
     </>
   );
 };
--- src/components/StarWarsIntro/styled.tsx
+++ src/components/StarWarsIntro/styled.tsx
@@ -1,4 +1,19 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+export const fadeAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  20% {
+    opacity: 1;
+  }
+  80% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
 
 export const TextIntro = styled.h1`
   font-family: "News Cycle", sans-serif;
@@ -6,16 +21,26 @@ export const TextIntro = styled.h1`
   font-weight: 400;
   letter-spacing: 0.1em;
   font-size: 36px;
-  /*  opacity: 0; */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: ${fadeAnimation} 5s linear;
+  opacity: 0;
 `;
 
-export const ContainerParagraph = styled.section`
+export const ContainerParagraph = styled.section<{ fade: boolean }>`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   position: absolute;
   left: 15%;
   width: 70%;
   z-index: 1;
   transform-origin: 50%;
   transform: perspective(300px) rotateX(20deg);
+  opacity: ${(props) => (props.fade ? 0 : 1)};
+  transition: opacity 1s ease;
 `;
 
 export const Paragraph = styled.p`
@@ -27,4 +52,19 @@ export const Paragraph = styled.p`
   line-height: 1.1em;
   width: 70%;
   font-size: 36.35px;
+  opacity: 0;
+  animation: fade-in 1s forwards;
+  animation-delay: 0s;
+  animation-timing-function: ease-in;
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+      transform: translateY(10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;

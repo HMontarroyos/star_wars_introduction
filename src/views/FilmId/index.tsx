1fc197f7783b25136c0cc11ad12de777c06b3b19
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-07-03 11:38:41 -0300

    feat: add news films in view

--- src/views/FilmId/index.tsx
+++ src/views/FilmId/index.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
-import * as S from "./styled";
-import { getFilmById } from "../../server/index";
-import { Loading, StarWarIntro } from "../../components";
 import { useParams } from "react-router-dom";
+import { getFilmById } from "../../server/index";
+import { othersFilmsStarWars } from "../../global/const";
+import { Loading, StarWarIntro } from "../../components";
 
 const FilmId: React.FC = () => {
   const [film, setFilm] = useState<any>();
   const { id } = useParams<{ id: string }>();
 
-
   useEffect(() => {
     async function fetchFilms(): Promise<void> {
       try {
-        const _film = await getFilmById(id)
-        setFilm(_film);
+        const _id = id && parseInt(id);
+        if (typeof _id === "number" && _id >= 7) {
+          const filmSelected = othersFilmsStarWars.find(
+            (filmActual) => filmActual.episode_id === _id
+          );
+          setFilm(filmSelected);
+        } else {
+          const _film = await getFilmById(id);
+          setFilm(_film);
+        }
       } catch (error) {
         console.error(error);
       }
@@ -22,10 +29,13 @@ const FilmId: React.FC = () => {
   }, []);
   return (
     <>
-    {console.log("FILM", film)}
       {film ? (
         <>
-          <StarWarIntro description={film.opening_crawl} subTitle={film.title} numberEpisode={film.episode_id}/>
+          <StarWarIntro
+            description={film.opening_crawl}
+            subTitle={film.title}
+            numberEpisode={film.episode_id}
+          />
         </>
       ) : (
         <Loading />

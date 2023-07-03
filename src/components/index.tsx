162373e1851cc5e734cab9d872f8d2cfe9e6818c
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-07-02 21:28:46 -0300

    fix: adjust import components crawl

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,9 +2,18 @@ import { lazy } from "react";
 
 import Cursor from "./Cursor";
 import Poster from "./Poster";
+import Crawl from "./Crawl";
 const Header = lazy(async () => await import("./Header"));
 const StarsBackground = lazy(async () => await import("./StarsBackground"));
 const StarWarIntro = lazy(async () => await import("./StarWarsIntro"));
 const Loading = lazy(async () => await import("./Loading"));
 
-export { Cursor, Poster, Loading, Header, StarsBackground, StarWarIntro };
+export {
+  Cursor,
+  Poster,
+  Crawl,
+  Loading,
+  Header,
+  StarsBackground,
+  StarWarIntro,
+};

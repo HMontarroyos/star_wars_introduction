5b1b294fefebd2940315acbed3f649674f5928ca
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-07-03 11:38:52 -0300

    feat: add method add new films

--- src/global/const.tsx
+++ src/global/const.tsx
@@ -11,6 +11,45 @@ export const PostersStarWarsFilms: any = {
     "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6763_AL_.jpg",
   ReturnoftheJedi:
     "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.6763_AL_.jpg",
+  TheForceAwakens:
+    "https://vitralizado.com/wp-content/uploads/2015/10/EpVII.jpg",
+  TheLastJedi:
+    "https://lumiere-a.akamaihd.net/v1/images/swtlj_imax_oversize_1-sht_v6_lg_d4edae12.jpeg",
+  TheRiseOfSkywalker:
+    "https://universoreverso.com.br/wp-content/uploads/2019/12/star-wars-9-poster.jpg",
   NoImage:
     "https://s2.glbimg.com/xIK2tpANlRYKSIIrUYEnndmeW4s=/smart/e.glbimg.com/og/ed/f/original/2015/11/17/darth-vader-star-wars.jpg",
 };
+
+export const othersFilmsStarWars = [
+  {
+    title: "The Force Awakens",
+    opening_crawl: [
+      "Luke Skywalker has vanished. In his absence, the sinister FIRST ORDER has risen from the ashes of the Empire and will not rest until Skywalker, the last Jedi, has been destroyed.",
+      "With the support of the REPUBLIC, General Leia Organa leads a brave RESISTANCE. She is desperate to find her brother Luke and gain his help in restoring peace and justice to the galaxy.",
+      "Leia has sent her most daring pilot on a secret mission to Jakku, where an old ally has discovered a clue to Luke’s whereabouts ....",
+    ],
+    episode_id: 7,
+    url: "https://swapi.dev/api/films/7/",
+  },
+  {
+    title: "The last Jedi",
+    opening_crawl: [
+      "The FIRST ORDER reigns. Having decimated the peaceful Republic, Supreme Leader Snoke now deploys his merciless legions to seize military control of the galaxy.",
+      "Only General Leia Organa's band of RESISTANCE fighters stand against the rising tyranny, certain that Jedi Master Luke Skywalker will return and restore a spark of hope to the fight.",
+      "But the Resistance has been exposed. As the First Order speeds toward the rebel base, the brave heroes mount a desperate escape....",
+    ],
+    episode_id: 8,
+    url: "https://swapi.dev/api/films/8/",
+  },
+  {
+    title: "The Rise Of Skywalker",
+    opening_crawl: [
+      "The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE. ",
+      "GENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER. ",
+      "Meanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power….",
+    ],
+    episode_id: 9,
+    url: "https://swapi.dev/api/films/9/",
+  },
+];
